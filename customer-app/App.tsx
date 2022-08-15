0aaa6aafe8d618af8311b2673bf4dbfa6f1cbef5
Snehanshu <hello@snehanshu.com>
2022-08-15 13:05:44 +0530

    npm run prebuild

--- customer-app/App.tsx
+++ customer-app/App.tsx
@@ -1,7 +1,20 @@
 import { QueryClientProvider } from 'react-query';
+import messaging from '@react-native-firebase/messaging';
+import type { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
 import { queryClient } from './queries/client';
 import { Routes } from './routes';
 import { trpc, trpcClient } from './utils/trpc';
+
+// Note that an async function or a function that returns a Promise
+// is required for both subscribers.
+async function onMessageReceived(
+  message: FirebaseMessagingTypes.RemoteMessage,
+) {
+  console.log('Message received', message);
+}
+
+messaging().onMessage(onMessageReceived);
+messaging().setBackgroundMessageHandler(onMessageReceived);
 
 export default function App() {
   return (
